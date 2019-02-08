ca5ff219fe811a1d1f55c817f6ca271cdf33e3e3
Julien Desrosiers <juliend2@gmail.com>
2019-02-08 07:17:05 -0500

    Check the domain's record type, resolving IP, status code, and redirect destination

--- check_dns.rb
+++ check_dns.rb
@@ -1,5 +1,10 @@
 require 'resolv'
 
+require "net/https"
+require "uri"
+require 'open-uri' 
+
+
 class DnsCheck
 	attr_reader :host
 	def initialize(host)
@@ -44,6 +49,55 @@ class DnsCheck
 
 end
 
-puts DnsCheck.new('juliendesrosiers.ca').ip
-puts DnsCheck.new('test.juliendesrosiers.ca').cname?
-puts DnsCheck.new('www.juliendesrosiers.ca').ip
+domains = %w[
+  juliendesrosiers.ca juliendesrosiers.com
+  opto.com optosys.ca tv.opto.com web.opto.com
+]
+
+def check_status(uri)
+  http = Net::HTTP.new(uri.host, uri.port)
+  http.use_ssl = true
+
+  request = Net::HTTP::Get.new(uri.request_uri)
+  res = http.request(request)
+
+  res.code
+end
+
+def check_redirect_location(uri)
+  r = Net::HTTP.get_response(uri)
+  r.header['location']
+end
+
+def check_domain(domain)
+  uri = URI.parse("https://#{domain}/")
+  record_type = :CNAME if DnsCheck.new(domain).cname?
+  record_type = :A if DnsCheck.new(domain).a?
+  record_ip = DnsCheck.new(domain).ip
+  status_code = check_status(uri)
+  redirect_to = if (300..399).include?(status_code.to_i)
+                  check_redirect_location(uri)
+                else
+                  nil
+                end
+
+  puts "DOMAIN: #{domain},\tRECORD: #{record_type},\tIP: #{record_ip},\tSTATUS: #{status_code}#{redirect_to ? ",\tREDIRECTS: #{redirect_to}" : ''}"
+end
+
+def check(domain, is_subdomain = false)
+  uri = URI.parse("https://#{domain}/")
+
+  check_domain(domain)
+  check_domain("www.#{domain}") unless is_subdomain
+  puts "-------------------------------------------------"
+end
+
+domains.each do |domain|
+  domain_parts = domain.split('.')
+  if domain_parts.size > 2
+    check(domain, true)
+  else
+    check(domain)
+  end
+end
+
